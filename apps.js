a4d00aba82f00a8a7831772d2298bf000e3eaa49
Dennisovich-03 <54091928+Dennisovich-03@users.noreply.github.com>
2019-10-09 00:27:16 +0530

    install2

--- apps.js
+++ apps.js
@@ -1,3 +1,15 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    btnAdd.style.display = 'block';
+
+});
+
+
 btnAdd.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
     btnAdd.style.display = 'none';
